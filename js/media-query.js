b382133a9b2d988a1b99240f94385408896a6e89
daniel22alejandro <danniel10yara@gmail.com>
2023-08-01 16:45:58 -0500

    add

--- js/media-query.js
+++ js/media-query.js
@@ -1,14 +1,15 @@
-let logoHamburguer = document.getElementById("logoHamburguer");
-let mainMenu = document.getElementById("mainMenu");
+let logoHamburguer= document.getElementById("logoHamburguer")
+let mainMenu= document.getElementById("mainMenu")
 
-logoHamburguer.addEventListener("click", function () {
-    if (mainMenu.classList.contains("main-menu")) {
-    mainMenu.classList.add("main-menu-block");
-    mainMenu.classList.remove("main-menu");
+logoHamburguer.addEventListener("click",function() {
+    if (mainMenu.classList.contains("main-menu")){
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu");
     }
-    else {
-        mainMenu.classList.remove("main-emnu-block");
+    else{
+        mainMenu.classList.remove("main-menu-block");
         mainMenu.classList.add("main-menu");
-    }
 
-});
+    }
+    
+})
