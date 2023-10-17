5f3c7fe489ffe296e6b9fc9d84df540911f853d6
daniel22alejandro <danniel10yara@gmail.com>
2023-10-17 14:27:23 -0500

    es tiempo

--- js/carrito.js
+++ js/carrito.js
@@ -1,16 +1,41 @@
 let boxPlus = document.getElementById("boxPlus")
+let boxMinus = document.getElementById("boxMinus")
 let boxUnidades = document.getElementById("boxUnidades")
+let subTotal = document.getElementById("subTotal")
+const valorUnidad = 85000
+const valorUnidadStr = Intl.NumberFormat("de-De").format(valorUnidad)
+document.getElementById("precioUnidad").innerHTML = "$" + valorUnidadStr
 
 boxPlus.addEventListener("click", function() {
     let auxUnidades = parseInt(boxUnidades.innerHTML)
     if (auxUnidades < 10 ) {
         boxUnidades.innerHTML = (auxUnidades + 1).toString()
+        let valorsubTotal = parseInt(boxUnidades.innerHTML) * valorUnidad
+        subTotal.innerHTML = Intl.NumberFormat("de-De").format (valorsubTotal)
+        if (boxUnidades.innerHTML == 1) {
+            document.getElementById("cantProductos").innerHTML = boxUnidades.innerHTML + " Producto"
+        }
+        else {
+            document.getElementById("cantProductos").innerHTML = boxUnidades.innerHTML + " Productos"
+        }
+
     }
 })
 boxMinus.addEventListener("click", function() {
     let auxUnidades = parseInt(boxUnidades.innerHTML)
     if (auxUnidades > 0 ) {
         boxUnidades.innerHTML = (auxUnidades - 1).toString()
+        let valorsubTotal = parseInt(boxUnidades.innerHTML) * valorUnidad
+        subTotal.innerHTML = Intl.NumberFormat("de-De").format (valorsubTotal)
+
+
+        if (boxUnidades.innerHTML == 1) {
+            document.getElementById("cantProductos").innerHTML = boxUnidades.innerHTML + " Producto"
+        }
+        else {
+            document.getElementById("cantProductos").innerHTML = boxUnidades.innerHTML + " Productos"
+        }
+
     }
 })
 
