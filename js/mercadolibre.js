1ddf057a53e2bce259860f2c825bcb7948579ae5
daniel22alejandro <danniel10yara@gmail.com>
2023-11-14 16:25:21 -0500

    subiendo cambios

--- js/mercadolibre.js
+++ js/mercadolibre.js
@@ -1,4 +1,36 @@
+class Automovil {
+    constructor (marca, modelo, precio, anio, kilometros, ciudad, imagen){
+        this.marca = marca; 
+        this.modelo = modelo; 
+        this.precio = precio; 
+        this.anio = anio; 
+        this.kilometros = kilometros; 
+        this.ciudad = ciudad;
+        this.imagen = imagen;  
+    } 
+}
+
+let auto1 = new Automovil("Ducati", "Multistrada V4", "$180.000.000", "2022", "4.302 km", "Medellin - Antioquia", "img/Ducati_multiestrada.webp");
+let auto2 = new Automovil("Beneli", "Trk 502x 2023", "$31.400.000", "2023", "8.512 km", "Envigado - Antioquia", "img/Benelli-Trk-502x-2023.webp");
+let auto3 = new Automovil("Kawazaki", "Zh2", "$124.990.000", "2023", "0 km", "Usaquen - Bogota D.C.", "img/Kawasaki-Zh2.webp");
+let auto4 = new Automovil("BMW", "S1000rr", "$145.000.000", "2023", "1.800 km", "Medellin - Antioquia", "img/Bmw-S1000rr.webp");
+
+
+
+
+
 let boxProductos = document.getElementById("boxProductos")
+
+
+cargarAutomovil(auto1)
+cargarAutomovil(auto2)
+cargarAutomovil(auto3)
+cargarAutomovil(auto4)
+
+
+
+
+function cargarAutomovil(auto){
 
 
 // contenedor para productos individuales
@@ -13,7 +45,7 @@ boxProducto.appendChild(boxImagen)
 boxImagen.setAttribute("class", "box-imagen")
 let imagenProducto = document.createElement("img")
 boxImagen.appendChild(imagenProducto)
-imagenProducto.setAttribute("src", "img/Ducati_multiestrada.webp")
+imagenProducto.setAttribute("src", auto.imagen)
 imagenProducto.setAttribute("class", "imagen-producto")
 
 
@@ -27,5 +59,43 @@ infoProducto.setAttribute("class", "box-info-producto")
 
 let marcaProducto = document.createElement("label")
 infoProducto.appendChild(marcaProducto)
-let nodoMarcaProducto = document.createTextNode("Ducati Multistrada V4")
+let nodoMarcaProducto = document.createTextNode(auto.marca)
 marcaProducto.appendChild(nodoMarcaProducto)
+marcaProducto.setAttribute("class", "marca-producto")
+
+
+let modeloProducto = document.createElement("label")
+infoProducto.appendChild(modeloProducto)
+let nodoModeloProducto = document.createTextNode(auto.modelo)
+modeloProducto.appendChild(nodoModeloProducto)
+modeloProducto.setAttribute("class", "modelo-producto")
+
+
+let precioProducto = document.createElement("label")
+infoProducto.appendChild(precioProducto)
+let nodoPrecioProducto = document.createTextNode(auto.precio)
+precioProducto.appendChild(nodoPrecioProducto)
+precioProducto.setAttribute("class", "precio-producto")
+
+
+let anioProducto = document.createElement("label")
+infoProducto.appendChild(anioProducto)
+let nodoAnioProducto = document.createTextNode(auto.anio + " · ")
+anioProducto.appendChild(nodoAnioProducto)
+anioProducto.setAttribute("class", "anio-producto")
+
+let kiloProducto = document.createElement("label")
+infoProducto.appendChild(kiloProducto)
+let nodoKiloProducto = document.createTextNode(auto.kilometros + " · ")
+kiloProducto.appendChild(nodoKiloProducto)
+kiloProducto.setAttribute("class", "kilo-producto")
+
+
+let ciudProducto = document.createElement("label")
+infoProducto.appendChild(ciudProducto)
+let nodoCiudProducto = document.createTextNode(auto.ciudad)
+ciudProducto.appendChild(nodoCiudProducto)
+ciudProducto.setAttribute("class", "ciud-producto")
+
+
+}
