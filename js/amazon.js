b0fabcbd9cb85395b423b10f7613b071d70e1bc2
daniel22alejandro <danniel10yara@gmail.com>
2023-12-12 16:39:21 -0500

    chaiarasasd

--- js/amazon.js
+++ js/amazon.js
@@ -5,8 +5,16 @@ let calificacion4 = document.getElementById("calificacion4")
 let calificacion5 = document.getElementById("calificacion5")
 let btnGraficar = document.getElementById("btnGraficar")
 
+let porcentajeCalificacion1 = document.getElementById("porcentajeCalificacion1")
+
+
+
 btnGraficar.addEventListener("click", function() {
-    let sumaCalificacion = Number(calificacion1.value) + Number(calificacion2.value) + Number(calificacion3.value) + Number(calificacion4.value) + Number(calificacion5.value)
+    let sumaCalificacion = Number(calificacion1.value) + 
+    Number(calificacion2.value) + 
+    Number(calificacion3.value) +
+    Number(calificacion4.value) + 
+    Number(calificacion5.value)
 /*     alert(sumaCalificacion) */
 
     let promCalificacion1 = calificacion1.value * 100  / sumaCalificacion
@@ -14,25 +22,14 @@ btnGraficar.addEventListener("click", function() {
     let promCalificacion3 = calificacion3.value * 100  / sumaCalificacion
     let promCalificacion4 = calificacion4.value * 100  / sumaCalificacion
     let promCalificacion5 = calificacion5.value * 100  / sumaCalificacion
-    alert(promCalificacion1)
-/*     calificacion1.value
-    calificacion2.value
-    calificacion3.value
-    calificacion4.value
-    calificacion5.value
+    console.log(promCalificacion1.toFixed(0))
 
-    let sumar1 = (calificacion1.value/100)
-    alert(sumar1)
+    porcentajeCalificacion1.style.width = (promCalificacion1 * 2) + "px"
+    let spanPorcentaje = document.createElement("span")
+    boxTextoCalificacion.appendChild(spanPorcentaje)
+    spanPorcentaje.innerHTML = promCalificacion1.toFixed(0) + "%"
+    spanPorcentaje.setAttribute("class", "span-porcentaje")
 
-    let sumar2 = (calificacion2.value/100)
-    alert(sumar2)
 
-    let sumar3 = (calificacion3.value/100)
-    alert(sumar3)
 
-    let sumar4 = (calificacion4.value/100)
-    alert(sumar4)
-
-    let sumar5 = (calificacion5.value/100)
-    alert(sumar5) */
-})
+})  
